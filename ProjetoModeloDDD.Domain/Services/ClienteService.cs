710ed981530bf476efea2cb48f7582d9b64e37ba
diegoma <diegomoreiraalves.92@gmail.com>
2018-05-13 22:36:06 -0300

    Initial commit
    - DDD example by Eduardo Pires

---
+++ ProjetoModeloDDD.Domain/Services/ClienteService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Domain.Interfaces.Repositories;
+using ProjetoModeloDDD.Domain.Interfaces.Services;
+
+namespace ProjetoModeloDDD.Domain.Services {
+    public class ClienteService : ServiceBase<Cliente>, IClienteService
+    {
+        private readonly IClienteRepository _clienteRepository;
+
+        public ClienteService(IClienteRepository clienteRepository)
+            : base(clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+
+        public IEnumerable<Cliente> ObterClientesEspeciais(IEnumerable<Cliente> clientes)
+        {
+            return _clienteRepository.ObterClientesEspeciais(clientes);
+        }
+    }
+}
---
+++ ProjetoModeloDDD.Infra.Data/Repositories/ClienteRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Domain.Interfaces.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoModeloDDD.Infra.Data.Repositories
+{
+    public class ClienteRepository : RepositoryBase<Cliente>, IClienteRepository
+    {
+        public IEnumerable<Cliente> ObterClientesEspeciais(IEnumerable<Cliente> clientes) {
+            return clientes.Where(c => c.ClienteEspecial(c));
+        }
+    }
+}
